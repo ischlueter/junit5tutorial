52ebf02cc1f5475139c0659f439f26830cc856f5
Ingo Schlueter <ingo.schlueter@lhind.dlh.de>
2019-12-27 08:22:38 +0100

    after blog 2

--- src/test/java/org/priv/ingo/JUnit5ExampleTest.java
+++ src/test/java/org/priv/ingo/JUnit5ExampleTest.java
@@ -1,10 +1,50 @@
 package org.priv.ingo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@DisplayName("JUnit 5 Example")
 class JUnit5ExampleTest {
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("Before all test methods");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("Before each test method");
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("After each test method");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("After all test methods");
+  }
+
   @Test
   void justAnExample() {
+    System.out.println("Test 1");
   }
+
+  @Test
+  void justAnExample2() {
+    System.out.println("Test 2");
+  }
+
+  @Test
+  @DisplayName("First test")
+  void firstTest() {
+    System.out.println("First test method");
+  }
+
+  @Test
+  @DisplayName("Second test")
+  void secondTest() {
+    System.out.println("Second test method");
+  }
+
 }
 
